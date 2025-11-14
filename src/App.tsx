36d8877f81a5a639079e087aa6d1585af498fbbb
Agustin <agustin.oberti@bleett.com>
2025-11-14 17:27:19 -0300

    demo test

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>MANU BOTON</h1>
       <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
